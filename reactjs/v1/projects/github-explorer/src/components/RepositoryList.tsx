d58fbb8e7970162c0619d80b380a3d6784fbb186
Elias Gabriel <eliasgabrielcf@gmail.com>
2021-03-10 10:35:26 -0300

    Add 01 - Fundamentos do ReactJS

---
+++ reactjs/v1/projects/github-explorer/src/components/RepositoryList.tsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from 'react';
+
+import { RepositoryItem } from './RepositoryItem';
+
+import '../styles/repositories.scss';
+
+interface Repository {
+  name: string;
+  description: string;
+  html_url: string;
+}
+
+export function RepositoryList() {
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  useEffect(() => {
+    fetch('https://api.github.com/orgs/rocketseat/repos')
+      .then(response => response.json())
+      .then(data => setRepositories(data))
+  }, []);
+
+  console.log(repositories)
+
+  return (
+    <section className="repository-list">
+      <h1>Lista de repositórios</h1>
+
+      <ul>
+        {repositories.map(repository => {
+          return <RepositoryItem key={repository.name} repository={repository} />
+        })}
+      </ul>
+    </section>
+  );
+}
