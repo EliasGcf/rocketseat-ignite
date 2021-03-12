639da2d0e159482d2e2d197c835f1da485c0261a
Elias Gabriel <eliasgabrielcf@gmail.com>
2021-03-12 09:51:10 -0300

    nodejs: Finish challenge 03

---
+++ nodejs/v1/challenges/desafio-03-corrigindo-o-codigo/src/index.js
@@ -0,0 +1,76 @@
+const express = require('express');
+
+const { v4: uuid } = require('uuid');
+
+const app = express();
+
+app.use(express.json());
+
+const repositories = [];
+
+app.get('/repositories', (request, response) => {
+  return response.json(repositories);
+});
+
+app.post('/repositories', (request, response) => {
+  const { title, url, techs } = request.body;
+
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0,
+  };
+
+  repositories.push(repository);
+
+  return response.json(repository);
+});
+
+app.put('/repositories/:id', (request, response) => {
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  if (repositoryIndex < 0) {
+    return response.status(404).json({ error: 'Repository not found' });
+  }
+
+  repositories[repositoryIndex].title = title;
+  repositories[repositoryIndex].url = url;
+  repositories[repositoryIndex].techs = techs;
+
+  return response.json(repositories[repositoryIndex]);
+});
+
+app.delete('/repositories/:id', (request, response) => {
+  const { id } = request.params;
+
+  repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  if (repositoryIndex < 0) {
+    return response.status(404).json({ error: 'Repository not found' });
+  }
+
+  repositories.splice(repositoryIndex, 1);
+
+  return response.status(204).send();
+});
+
+app.post('/repositories/:id/like', (request, response) => {
+  const { id } = request.params;
+
+  repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  if (repositoryIndex < 0) {
+    return response.status(404).json({ error: 'Repository not found' });
+  }
+
+  const likes = ++repositories[repositoryIndex].likes;
+
+  return response.json({ likes });
+});
+
+module.exports = app;
