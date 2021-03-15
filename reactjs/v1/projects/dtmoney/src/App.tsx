dd98620083edd68de1259d8a76255405370b390e
Elias Gabriel <eliasgabrielcf@gmail.com>
2021-03-15 11:34:32 -0300

    reactjs: Add 02 - Primeira aplicação web com ReactJS

---
+++ reactjs/v1/projects/dtmoney/src/App.tsx
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+import Modal from 'react-modal';
+
+import { Header } from './components/Header';
+import { Dashboard } from './components/Dashboard';
+import { TransactionsProvider } from './hooks/useTransactions';
+import { NewTransactionModal } from './components/NewTransactionModal';
+
+import { GlobalStyle } from './styles/global';
+
+Modal.setAppElement('#root');
+
+export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false);
+  }
+
+  return (
+    <TransactionsProvider>
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
+      <Dashboard />
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
+      <GlobalStyle />
+    </TransactionsProvider>
+  );
+}
---
+++ reactjs/v1/projects/dtmoney/src/hooks/useTransactions.tsx
@@ -0,0 +1,53 @@
+import { createContext, ReactNode, useContext, useEffect, useState } from 'react';
+
+import { api } from '../services/api';
+
+interface Transaction {
+  id: string;
+  title: string;
+  type: string;
+  amount: number;
+  category: string;
+  createdAt: string;
+}
+
+type TransactionInput = Omit<Transaction, 'id' | 'createdAt'>
+
+interface  TransactionsProviderProps {
+  children: ReactNode;
+}
+
+interface TransactionsContextData {
+  transactions: Transaction[];
+  createTransaction(transaction: TransactionInput): Promise<void>;
+}
+
+const TransactionsContext = createContext<TransactionsContextData>({} as TransactionsContextData);
+
+export function TransactionsProvider({ children }: TransactionsProviderProps) {
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+
+  useEffect(() => {
+    api.get('transactions')
+      .then(response =>setTransactions(response.data.transactions));
+  }, []);
+
+  async function createTransaction(transactionInput: TransactionInput) {
+    const response = await api.post('transactions', transactionInput);
+    const { transaction } = response.data;
+
+    setTransactions([...transactions, transaction]);
+  }
+
+  return (
+    <TransactionsContext.Provider value={{ transactions, createTransaction }}>
+      {children}
+    </TransactionsContext.Provider>
+  )
+}
+
+export function useTransactions() {
+  const context = useContext(TransactionsContext);
+
+  return context;
+}
