110679b16da1a63212d1480dbd532d6990c44a69
Elias Gabriel <eliasgabrielcf@gmail.com>
2021-03-16 14:26:37 -0300

    nodejs: Finish challenge 04

---
+++ nodejs/v1/challenges/desafio-04-introducao-ao-SOLID/src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from 'express';
+
+import { ShowUserProfileUseCase } from './ShowUserProfileUseCase';
+
+class ShowUserProfileController {
+  constructor(private showUserProfileUseCase: ShowUserProfileUseCase) {}
+
+  handle(request: Request, response: Response): Response {
+    const { user_id } = request.params;
+
+    try {
+      const user = this.showUserProfileUseCase.execute({ user_id });
+
+      return response.json(user);
+    } catch (err) {
+      return response.status(404).json({ error: err.message });
+    }
+  }
+}
+
+export { ShowUserProfileController };
---
+++ nodejs/v1/challenges/desafio-04-introducao-ao-SOLID/src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
@@ -0,0 +1,24 @@
+import { User } from '../../model/User';
+import { IUsersRepository } from '../../repositories/IUsersRepository';
+
+interface IRequest {
+  user_id: string;
+}
+
+class TurnUserAdminUseCase {
+  constructor(private usersRepository: IUsersRepository) {}
+
+  execute({ user_id }: IRequest): User {
+    const user = this.usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new Error('User not found');
+    }
+
+    const userAsAdmin = this.usersRepository.turnAdmin(user);
+
+    return userAsAdmin;
+  }
+}
+
+export { TurnUserAdminUseCase };
