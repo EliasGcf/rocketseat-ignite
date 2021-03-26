e5bf58e32bd11208ffc8400a0c8cb458e18514f2
Elias Gabriel <eliasgabrielcf@gmail.com>
2021-03-26 13:43:17 -0300

    nodejs: Finish challenge 06

---
+++ nodejs/v1/challenges/desafio-06-database-queries/src/modules/users/repositories/implementations/UsersRepository.ts
@@ -0,0 +1,35 @@
+import { getRepository, Repository } from 'typeorm';
+
+import { IFindUserWithGamesDTO, IFindUserByFullNameDTO } from '../../dtos';
+import { User } from '../../entities/User';
+import { IUsersRepository } from '../IUsersRepository';
+
+export class UsersRepository implements IUsersRepository {
+  private repository: Repository<User>;
+
+  constructor() {
+    this.repository = getRepository(User);
+  }
+
+  async findUserWithGamesById({
+    user_id,
+  }: IFindUserWithGamesDTO): Promise<User> {
+    return this.repository.findOneOrFail(user_id, {
+      relations: ['games'],
+    });
+  }
+
+  async findAllUsersOrderedByFirstName(): Promise<User[]> {
+    return this.repository.query('SELECT * FROM users ORDER BY first_name ASC');
+  }
+
+  async findUserByFullName({
+    first_name,
+    last_name,
+  }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
+    return this.repository.query(
+      'SELECT * FROM users WHERE LOWER(first_name) = LOWER($1) AND LOWER(last_name) = LOWER($2)',
+      [first_name, last_name],
+    );
+  }
+}
