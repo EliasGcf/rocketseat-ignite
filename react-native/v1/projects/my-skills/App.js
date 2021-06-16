9298f2bf4bc31f55c08bef4a34f919381c14e3f3
Elias Gabriel <eliasgabrielcf@gmail.com>
2021-06-15 22:14:11 -0300

    ignite(react-native): Como fazer reload na Aplicação e Fast Refresh

--- react-native/v1/projects/my-skills/App.js
+++ react-native/v1/projects/my-skills/App.js
@@ -71,8 +71,7 @@ const App: () => Node = () => {
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
           <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
+            Voltar o que estava antes
           </Section>
           <Section title="See Your Changes">
             <ReloadInstructions />
