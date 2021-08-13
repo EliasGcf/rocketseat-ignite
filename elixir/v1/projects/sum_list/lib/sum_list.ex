70bc563f8664f45b43dd6e2d8039dfab2eed0e9e
Elias Gabriel <eliasgabrielcf@gmail.com>
2021-08-12 23:35:41 -0300

    ignite(elixir): use Enum.map

--- elixir/v1/projects/sum_list/lib/sum_list.ex
+++ elixir/v1/projects/sum_list/lib/sum_list.ex
@@ -1,6 +1,8 @@
 defmodule SumList do
   def call(list), do: sum(list, 0)
 
+  def call_enum(list), do: Enum.map(list, fn elem -> elem + 1 end)
+
   defp sum([], acc), do: acc
 
   defp sum([head | tail], acc) do
