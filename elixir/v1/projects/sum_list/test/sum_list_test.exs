94d60c03c57d0fdf3b85a88d960beace5883ad32
Elias Gabriel <eliasgabrielcf@gmail.com>
2021-08-12 23:31:17 -0300

    ignite(elixir): create test to SumList module

--- elixir/v1/projects/sum_list/test/sum_list_test.exs
+++ elixir/v1/projects/sum_list/test/sum_list_test.exs
@@ -1,8 +1,15 @@
 defmodule SumListTest do
   use ExUnit.Case
-  doctest SumList
 
-  test "greets the world" do
-    assert SumList.hello() == :world
+  describe "call/1" do
+    test "returns the list sum" do
+      list = [1, 2, 3]
+
+      response = SumList.call(list)
+
+      expect_response = 6
+
+      assert response == expect_response
+    end
   end
 end
