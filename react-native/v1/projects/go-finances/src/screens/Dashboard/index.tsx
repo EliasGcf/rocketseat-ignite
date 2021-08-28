de5b280bf766d53c986603ba5258a34bd9457c4c
Elias Gabriel <eliasgabrielcf@gmail.com>
2021-08-28 15:35:43 -0300

    ignite(react-native, go-finances): add styled-components

---
+++ react-native/v1/projects/go-finances/src/screens/Dashboard/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+import { Container } from './styles';
+
+export function Dashboard() {
+  return (
+    <Container>
+      <Text>Dashboard</Text>
+    </Container>
+  );
+}
