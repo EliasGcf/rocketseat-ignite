c8a79c564091a2be2f92a6e90bea989bd6a5c8c3
Elias Gabriel <eliasgabrielcf@gmail.com>
2021-08-29 15:51:31 -0300

    ignite(react-native, go-finances): create Header in Dashboard page

--- react-native/v1/projects/go-finances/src/screens/Dashboard/index.tsx
+++ react-native/v1/projects/go-finances/src/screens/Dashboard/index.tsx
@@ -1,11 +1,38 @@
 import React from 'react';
+import { useTheme } from 'styled-components';
+import { StatusBar, TouchableOpacity } from 'react-native';
 
-import { Container, Title } from './styles';
+import {
+  Container,
+  Header,
+  UserAvatar,
+  UserInfo,
+  UserGreeting,
+  UserName,
+  UserTextContainer,
+  PowerIcon,
+} from './styles';
 
 export function Dashboard() {
+  const theme = useTheme();
+
   return (
     <Container>
-      <Title>Dashboard</Title>
+      <StatusBar barStyle="light-content" backgroundColor={theme.colors.primary} />
+      <Header>
+        <UserInfo>
+          <UserAvatar source={{ uri: 'https://github.com/EliasGcf.png' }} />
+
+          <UserTextContainer>
+            <UserGreeting>Olá,</UserGreeting>
+            <UserName>Elias Gabriel</UserName>
+          </UserTextContainer>
+
+          <TouchableOpacity style={{ marginLeft: 'auto' }} activeOpacity={0.7}>
+            <PowerIcon />
+          </TouchableOpacity>
+        </UserInfo>
+      </Header>
     </Container>
   );
 }
--- react-native/v1/projects/go-finances/src/screens/Dashboard/styles.ts
+++ react-native/v1/projects/go-finances/src/screens/Dashboard/styles.ts
@@ -1,13 +1,60 @@
-import styled from 'styled-components/native';
+import { getStatusBarHeight, isIphoneX } from 'react-native-iphone-x-helper';
+import styled, { css } from 'styled-components/native';
+import { Feather } from '@expo/vector-icons';
+
+const Row = styled.View`
+  flex-direction: row;
+`;
 
 export const Container = styled.View`
   flex: 1;
-  justify-content: center;
-  align-items: center;
-
   background: ${({ theme }) => theme.colors.background};
 `;
 
-export const Title = styled.Text`
+export const Header = styled.View`
+  width: 100%;
+  padding: 28px 0 0 0;
+  background: ${({ theme }) => theme.colors.primary};
+
+  ${isIphoneX() &&
+  css`
+    padding: ${getStatusBarHeight() + 28}px 0 0 0;
+  `}
+`;
+
+export const UserInfo = styled(Row)`
+  height: 48px;
+  padding: 0 24px 0 24px;
+  align-items: center;
+`;
+
+export const UserAvatar = styled.Image`
+  width: 48px;
+  height: 48px;
+  margin: 0 18px 0 0;
+  border-radius: 10px;
+`;
+
+export const UserTextContainer = styled.View`
+  align-items: flex-start;
+  justify-content: space-around;
+`;
+
+export const UserGreeting = styled.Text`
+  font-size: 18px;
+  font-family: ${({ theme }) => theme.fonts.poppins.regular};
+  color: #fff;
+`;
+
+export const UserName = styled.Text`
+  font-size: 18px;
   font-family: ${({ theme }) => theme.fonts.poppins.bold};
+  color: #fff;
+`;
+
+export const PowerIcon = styled(Feather).attrs(props => {
+  return { name: 'power' };
+})`
+  color: ${({ theme }) => theme.colors.secondary};
+  font-size: 24px;
 `;
