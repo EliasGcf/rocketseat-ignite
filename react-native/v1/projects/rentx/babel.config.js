3cd4aaa3c29f6a78094efe587503a57dbd699097
Elias Gabriel <eliasgabrielcf@gmail.com>
2021-09-20 21:37:26 -0300

    ignite(react-native, rentx): setup typescript paths

--- react-native/v1/projects/rentx/babel.config.js
+++ react-native/v1/projects/rentx/babel.config.js
@@ -1,7 +1,18 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['react-native-reanimated/plugin']
+    plugins: [
+      'react-native-reanimated/plugin',
+      [
+        'module-resolver',
+        {
+          extensions: ['.ts', '.tsx'],
+          alias: {
+            src: './src',
+          },
+        },
+      ],
+    ],
   };
 };
