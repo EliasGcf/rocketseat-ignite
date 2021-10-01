088293bfd4994dce7552d06d9dbe418f34069ab7
Elias Gabriel <eliasgabrielcf@gmail.com>
2021-09-30 22:11:45 -0300

    ignite(react-native, rentx): increase header left button size

--- react-native/v1/projects/rentx/src/screens/CarDetails/HeaderLeftButton/index.tsx
+++ react-native/v1/projects/rentx/src/screens/CarDetails/HeaderLeftButton/index.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import { Feather } from '@expo/vector-icons';
 import { useTheme } from 'styled-components';
-import { BorderlessButton } from 'react-native-gesture-handler';
 import { useNavigation } from '@react-navigation/native';
+
+import { ButtonContainer } from './styles';
 
 export function HeaderLeftButton() {
   const theme = useTheme();
   const navigation = useNavigation();
 
   return (
-    <BorderlessButton onPress={navigation.goBack}>
+    <ButtonContainer onPress={navigation.goBack}>
       <Feather name="chevron-left" size={24} color={theme.colors.aliases.text} />
-    </BorderlessButton>
+    </ButtonContainer>
   );
 }
---
+++ react-native/v1/projects/rentx/src/screens/CarDetails/HeaderLeftButton/styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components/native';
+import { BorderlessButton } from 'react-native-gesture-handler';
+
+export const ButtonContainer = styled(BorderlessButton)`
+  width: 48px;
+  height: 48px;
+  align-items: center;
+  justify-content: center;
+`;
