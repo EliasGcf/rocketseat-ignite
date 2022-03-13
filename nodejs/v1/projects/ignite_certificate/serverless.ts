b09d110d292895244d54a5b2c97846ba804dede6
Elias Gabriel <eliasgabrielcf@gmail.com>
2022-03-13 10:32:07 -0300

    feat: add serverless s3 local

--- nodejs/v1/projects/ignite_certificate/serverless.ts
+++ nodejs/v1/projects/ignite_certificate/serverless.ts
@@ -1,10 +1,14 @@
 import type { AWS } from '@serverless/typescript';
 
-
 const serverlessConfiguration: AWS = {
   service: 'ignitecertificate',
   frameworkVersion: '2',
-  plugins: ['serverless-esbuild', 'serverless-dynamodb-local', 'serverless-offline'],
+  plugins: [
+    'serverless-esbuild',
+    'serverless-dynamodb-local',
+    'serverless-s3-local',
+    'serverless-offline',
+  ],
   provider: {
     name: 'aws',
     runtime: 'nodejs14.x',
@@ -78,6 +82,10 @@ const serverlessConfiguration: AWS = {
         migrate: true,
       },
     },
+    s3: {
+      host: 'localhost',
+      directory: './tmp',
+    },
   },
   resources: {
     Resources: {
@@ -101,6 +109,13 @@ const serverlessConfiguration: AWS = {
               KeyType: 'HASH',
             },
           ],
+        },
+      },
+
+      bucketCertificates: {
+        Type: 'AWS::S3::Bucket',
+        Properties: {
+          BucketName: 'certificadoignite2021',
         },
       },
     },
